5b3692d02a1707a38d2a610bc73f7d72fd7b8263
Scott Molloy <scott.l.molloy@gmail.com>
2015-11-04 12:09:08 -0800

    Update so LESS always show colors
    When viewing help with <command> --help, colors were not appearing
    because they were only being used for "man" command. Now colors for LESS
    are exported globally. Leaving the man function there as an example.

--- bashrc.d/2-man-color.sh
+++ bashrc.d/2-man-color.sh
@@ -36,3 +36,11 @@ man() {
         LESS_TERMCAP_us=$'\E[04;38;5;173m' \
         man "$@"
 }
+
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;38;5;67m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[04;38;5;173m'
