88458901571f658bb947e341d6e5e2d027b782db
Scott Molloy <scott.molloy@cigna.com>
2016-01-20 18:16:55 -0800

    Adding more complex colors

--- bashrc.d/1-colors.sh
+++ bashrc.d/1-colors.sh
@@ -1,4 +1,4 @@
-# Setup color variables
+# Setup basic color variables
 BLACK="\[\033[0;30m\]"
 DGRAY="\[\033[1;30m\]"
 RED="\[\033[0;31m\]"
@@ -17,7 +17,16 @@ LGRAY="\[\033[0;37m\]"
 WHITE="\[\033[1;37m\]"
 NEUTRAL="\[\033[0m\]"
 
+
+# Setup complex color variables
+C_GREEN="\[\033[38;5;82m\]"
+C_BLUE="\[\033[38;5;75m\]"
+C_BROWN="\[\033[38;5;220m\]"
+C_PURPLE="\[\033[38;5;183m\]"
+
 export BLACK DGRAY RED LRED GREEN LGREEN BROWN YELLOW BLUE
 export LBLUE PURPLE LPURPLE CYAN LCYAN LGRAY WHITE NEUTRAL
+
+export C_GREEN C_BLUE C_BROWN C_PURPLE
 
 export TERM="xterm-256color"
