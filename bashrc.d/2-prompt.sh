fd811458ab3b443c4e829a03a17460083169a211
Scott Molloy <scott.l.molloy@gmail.com>
2016-01-23 17:28:58 -0800

    Updating PS1 prompt colors to 256 color pallete

--- bashrc.d/2-prompt.sh
+++ bashrc.d/2-prompt.sh
@@ -1,4 +1,4 @@
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto verbose"
-PS1="${debian_chroot:+($debian_chroot)}$LGREEN\u@\h$NEUTRAL:$LBLUE\w$LCYAN"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
+PS1="${debian_chroot:+($debian_chroot)}$C_GREEN\u@\h$NEUTRAL:$C_BLUE\w$C_BROWN"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
