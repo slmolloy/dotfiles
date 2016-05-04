00b6cbd2afee58e58db82876761390415e69ed54
Scott Molloy <scott.molloy@cigna.com>
2016-05-04 14:04:29 -0700

    Add prompt support for Darwin, Linux and Other

--- bashrc.d/2-prompt.sh
+++ bashrc.d/2-prompt.sh
@@ -1,4 +1,16 @@
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto verbose"
-PS1="${debian_chroot:+($debian_chroot)}$C_GREEN\u@\h$NEUTRAL:$C_BLUE\w$C_BROWN"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
+
+os=`uname -s`
+if [ $os == "Linux" ]; then
+    # Linux
+    PS1="${debian_chroot:+($debian_chroot)}$C_GREEN\u@\h$NEUTRAL:$C_BLUE\w$C_BROWN"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
+elif [ $os == "Darwin" ]; then
+    # Mac
+    PS1="$C_GREEN\u@\h$NEUTRAL:$C_BLUE\w$C_BROWN"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
+else
+    # Other
+    PS1="$C_GREEN\u@\h:\w"'$(__git_ps1 " (%s)")'"$NEUTRAL$ "
+fi
+unset os
