2f2920814d16bf469fc79ba940f01481690c6088
Scott Molloy <scott.molloy@cigna.com>
2016-05-04 13:47:49 -0700

    Update bashrc.d ssh to use $HOME instead of $USER

--- bashrc.d/2-ssh.sh
+++ bashrc.d/2-ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! ssh-add -l | grep -e "\/home\/$USER\/\.ssh\/id_rsa" >/dev/null
+if ! ssh-add -l | grep -e "$HOME\/\.ssh\/id_rsa" >/dev/null
 then
     ssh-add $HOME/.ssh/id_rsa
 fi
